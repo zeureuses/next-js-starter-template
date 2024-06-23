a28389a2c659d66194a36397c04d26ae944d7296
zeureuses <trenasanabolas@gmail.com>
2024-06-23 17:08:42 -0400

    1st

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { loadJsonData } from "../utils";
+
+export default function Home() {
+  const jsonData = loadJsonData();
+
+  return (
+    <main>
+      <h1>belekas zopa vps</h1>
+      <h1 className="font-mono">dvizuha</h1>
+    </main>
+  );
+}
